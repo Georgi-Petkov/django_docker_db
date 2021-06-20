83101ade9ae078c328c214842be77d3941379e4b
Georgi Petkov <2georgipetkov@gmail.com>
2021-06-20 13:25:50 +0200

    GTM installed and more quotes added

--- random_quote/get_quote/views.py
+++ random_quote/get_quote/views.py
@@ -4,19 +4,22 @@ from . models import Quote
 # Create your views here.
 
 def index(request):
-    return render(request,'get_quote/index.html',{
-        'quotes': Quote.objects.all()
+    quotes = Quote.objects.all()
+    return render(request,"get_quote/index.html", {
+        "quotes":quotes
     })
     
-quote_text = []
-quote_author = [] 
-random_quote = [] 
+
 def get_quote(request):
     random_quote = Quote.objects.filter(id=[np.random.choice(430, 1, replace=False)[0]][0]).first()
-    quote_author = random_quote.author
-    quote_text = random_quote.quote
     return render(request,"get_quote/quote.html", {
-        "quote_text":quote_text,
-        "quote_author": quote_author
+        "quote_text":random_quote.quote,
+        "quote_author": random_quote.author
+    })
+ 
+def id_quote(request):
+    quotes = Quote.objects.all()
+    return render(request,"get_quote/index.html", {
+        "quotes":quotes
     })
 
