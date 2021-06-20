f224bdeb532c7c757b54209730d6a3b9ea7ca4b2
georgi-petkov <2georgipetkov@gmail.com>
2021-06-20 11:35:43 +0000

    Changes from pythonanywhere

--- random_quote/random_quote/settings.py
+++ random_quote/random_quote/settings.py
@@ -11,12 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os
+#import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = '/home/GeorgiPetkov/django_docker_db/random_quote/static'
 
 
 # Quick-start development settings - unsuitable for production
